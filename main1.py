85632aefd0f4f0a6338ea4e7187a1c2054edf947
Kim <69445981+alpha216@users.noreply.github.com>
2022-09-24 21:17:48 -0500

    Class

--- main1.py
+++ main1.py
@@ -1,60 +1,60 @@
-import json, re, base64, asyncio, os 
+import json, re, base64, asyncio, os
 import logging, logging.config
 import requests, aiohttp, aiofiles
 from bs4 import BeautifulSoup
 
+
 class Req():
-    
+
     def __init__(self) -> None:
         self.title = ""
 
-    async def files(self, session, name, url ,li):
-        #files(session, name = '197', url = "https://img.imimggggg87878.com/", li = ["https://img.com/", "https://img.com/"])
+    async def files(self, session, name, url, li):
         try:
-            #Get image location number ex) 1
-            loc = int(li.index(url))+1
-            #Keep process if image wasn't made
+            # Get image location number ex) 1
+            loc = int(li.index(url)) + 1
+            # Keep process if image wasn't made
             # ./주술회전/197 - 1.jpg
             if not os.path.isfile(f'./{self.title}/{name}-{str(loc)}.jpg'):
                 logger.debug(f"{name} files started {url}")
-                #fetch image
+                # fetch image
                 async with session.get(url) as response:
-                        if response.status == 200:
-                            #save the image
-                            f = await aiofiles.open(f'./{self.title}/{name}-{str(loc)}.jpg', mode='wb')
-                            await f.write(await response.read())
-                            await f.close()
-                            logger.debug(f"{name}-{str(loc)}.jpg file finished")
-                        else: #Bad Request
-                            await asyncio.sleep(2)
-                            async with aiohttp.ClientSession() as session:
-                                self.files(session, name, url, li)
-                            
-            else: logger.debug(f"{name} files are already exist {url}")
-        except: #Timeout
+                    if response.status == 200:
+                        # save the image
+                        f = await aiofiles.open(f'./{self.title}/{name}-{str(loc)}.jpg', mode='wb')
+                        await f.write(await response.read())
+                        await f.close()
+                        logger.debug(f"{name}-{str(loc)}.jpg file finished")
+                    else:  # Bad Request
+                        await asyncio.sleep(2)
+                        async with aiohttp.ClientSession() as session:
+                            self.files(session, name, url, li)
+
+            else:
+                logger.debug(f"{name} files are already exist {url}")
+        except Exception as e:  # Timeout
+            logger.error(e)
             await asyncio.sleep(2)
             async with aiohttp.ClientSession() as session:
-                self.files(session, name, url, li)
-
+                await self.files(session, name, url, li)
 
     def parse(self, html, url):
         # parse(html,['주술회전 197화', 'https://manamoa26.com/bbs/'])
-        name = url[0] #주술회전 197화
-        name = name.split()[1].replace('화', '') #197
+        name = url[0]  # 주술회전 197화
+        name = name.split()[4].replace('화', '')  # 197
         logger.debug(f"{url[0]}-{name} parse started")
-        
+
         # find img_list in js on html & decode
         json_string = re.search(r'var manamoa_img = (.*?);', html, re.S).group(1)
-        res = base64.b64decode(json_string) 
+        res = base64.b64decode(json_string)
         # Parse
         soup = BeautifulSoup(res, "lxml")
-        #collect img 
+        # collect img
         passmydivs = soup.find_all("img", src=True)
-        img_list =[i["src"] for i in passmydivs]
-        
-        #['197', ["https://img.imimggggg87878.com/", "https://img.imimggggg87878.com/"]]
-        return [name, [i for i in img_list]]
+        img_list = [i["src"] for i in passmydivs]
 
+        # ['197', ["https://img.imimggggg87878.com/", "https://img.imimggggg87878.com/"]]
+        return [name, [i for i in img_list]]
 
     async def fetch(self, session, url):
         async with session.get(url) as response:
@@ -62,33 +62,32 @@ class Req():
 
     async def fetch_and_parse(self, session, url):
         # feach(session, ['주술회전 197화', 'https://manamoa26.com/bbs/'])
-        link = url[1] #'https://manamoa26.com/bbs/'
+        link = url[1]  # 'https://manamoa26.com/bbs/'
         logger.debug(f"{url[0]} started")
-        #feach html
+        # feach html
         html = await self.fetch(session, link)
         logger.debug(f"{url[0]} fetched")
         # run parse(html) in a separate thread, and resume this coroutine when it completes
         loop = asyncio.get_event_loop()
         # parse(html,['주술회전 197화', 'https://manamoa26.com/bbs/'])
         paras = await loop.run_in_executor(None, self.parse, html, url)
-        #['197', ["https://img.imimggggg87878.com/", "https://img.imimggggg87878.com/"]]
+        # ['197', ["https://img.imimggggg87878.com/", "https://img.imimggggg87878.com/"]]
         logger.debug(f"{url[0]} parse finished")
-        #fetch img files & Save
-        #files(session, '197', "https://img.imimggggg87878.com/", ["https://img.com/", "https://img.com/"])
+        # fetch img files & Save
+        # files(session, '197', "https://img.imimggggg87878.com/", ["https://img.com/", "https://img.com/"])
         return await asyncio.gather(
             *(self.files(session, paras[0], url, paras[1]) for url in paras[1])
         )
-
 
     async def main(self):
         print(self.title)
         print("Title: ", self.title)
         # Make directory if it dosen't exist
         # if not self.title in os.listdir('./'):
-        #     await os.mkdir(f'./{self.title}')
-    
+        # os.mkdir(f'./{self.title}')
+
         logger.info("main start")
-        #Make session & feach_and_parse
+        # Make session & feach_and_parse
         async with aiohttp.ClientSession() as session:
             return await asyncio.gather(
                 # feach(session, ['주술회전 197화', 'https://manamoa26.com/bbs/'])
@@ -96,30 +95,33 @@ class Req():
             )
 
     def episode_lists(self, ur):
-            html = requests.get(ur).text
-            #parse
-            soup = BeautifulSoup(html, "lxml")
-            #title
-            self.title = soup.find("meta", property="og:title")['content'].strip()
-            #Epsiode Name & Link
-            passmydivs = soup.find_all("a", {"class": "item-subject"}, href=True)
-            linkss = [[i.contents[0].replace('\n', '').rstrip(), i['href']] for i in passmydivs]
-            
-            # return [['주술회전 197화', 'https://manamoa26.com/bbs/'], ['주술회전 196화', 'https://manamoa26.com/bbs/']]
-            return linkss
+        logger.info(f"{f} make list")
+        html = requests.get(ur).text
+        # parse
+        soup = BeautifulSoup(html, "lxml")
+        # title
+        self.title = soup.find("meta", property="og:title")['content'].strip()
+        # Epsiode Name & Link
+        passmydivs = soup.find_all("a", {"class": "item-subject"}, href=True)
+        linkss = [[i.contents[0].replace('\n', '').rstrip(), i['href']] for i in passmydivs]
+
+        # return [['주술회전 197화', 'https://manamoa26.com/bbs/'], ['주술회전 196화', 'https://manamoa26.com/bbs/']]
+        return linkss
+
 
 if __name__ == "__main__":
-    #Logger Setup
+    # Logger Setup
     with open('logging.json', 'rt') as f: config = json.load(f)
     logging.config.dictConfig(config)
     logger = logging.getLogger()
-    
-    #Make Class
+
+    # Make Class
     make = Req()
-    #Page link
+    # Page link
     link = input("Link: ")
     # return [['주술회전 197화', 'https://manamoa26.com/bbs/'], ['주술회전 196화', 'https://manamoa26.com/bbs/']]
     linksss = make.episode_lists(link.strip())
-    #Run Main
+    logger.info('Epsiode list finished')
+    # Run Main
     asyncio.run(make.main())
     logger.info("finished \n")
